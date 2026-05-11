07a9223e126135fd8217d1e58b802172962c7667
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-05-11 17:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const form = document.getElementById("myForm");
+
+form.addEvnetListner('submit' , function (e) {
+	event.preventDefault();
+
+	const age = document.getElementById('age').value;
+	const name = document.getElementById('name').value;
+
+	if(age === '' || name = ''){
+		alert('please enter valid details')
+		return;
+	}
+
+	const votingPromise = new Promise((resolve, reject) => {
+		setTimeout (() => {
+			if(age > 18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			}else{
+				reject(`Oh sorry . You aren't old enough.`)
+			}
+		}, 4000)
+	})
+
+	 votingPromise
+        .then((message) => {
+            alert(message);
+        })
+        .catch((error) => {
+            alert(error);
+    });
+})
